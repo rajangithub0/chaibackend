e94a49eaed414810d7f0e3802a9dcfa95a11969c
rajan <rajan.rana6@gamil.com>
2026-01-26 00:30:49 +0530

    all setup config and checking confim with postman

--- session5/src/app.js
+++ session5/src/app.js
@@ -1,4 +1,4 @@
-import { express } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
@@ -15,17 +15,18 @@ app.use(
   })
 );
 
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
+//routes import
 
+import userRouter from "./routes/user.routes.js";
 
+//router declaration
 
+app.use("/api/v1/users", userRouter);
 
-
-
-
-
+//http://localhost:8000/api/v1/users/register
 export { app };
--- session5/src/index.js
+++ session5/src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
